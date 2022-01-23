e91cceda3a59bc26a410d9d2160de9759dc4f9d7
danieleldjarn <danieleldjarn@gmail.com>
2022-01-23 01:42:03 +0000

    Add endpoint for current station data

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import json
 import requests
 import math
+from typing import List
 
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -21,29 +23,39 @@ def dist(lat1, lon1, lat2, lon2):
     return earthRadius * c
 
 @app.get('/')
-async def root():
-    return {'message': 'Hello world!'}
+async def front_page():
+    return RedirectResponse(url='/docs')
 
-@app.get('/getstation')
-async def getstation():
+@app.get('/get-closest-station')
+async def get_closest_station(lat: float, lon: float, measure: str):
+    """[summary]
+
+    Args:
+        lat (float): [description]
+        lon (float): [description]
+        measure (str): [description]
+
+    Returns:
+        [type]: [description]
+    """    
     # request format: [[lat, lon],[measure 1, measure 2, ...]]
-    requestList = [{'location': [64.141430, -21.914849], 'measures': ['H2S']}]
-
+    # requestList = [{'location': [64.141430, -21.914849], 'measures': ['H2S']}]
+    requestList = [{'location': [lat, lon], 'measure': [measure]}]
     stations = json.loads(requests.get('https://api.ust.is/aq/a/getStations').text)
 
     # find closest station with measure
-    minDist = 1000000
+    minDist = 1000000 # meters?
     id = -1
         
     for index, station in enumerate(stations):
-        lat = station['latitude']
-        lon = station['longitude']
+        # lat = station['latitude']
+        # lon = station['longitude']
         measures = station['parameters']
 
         for request in requestList: # process each request
             # filter out stations that don't measure the request
             hasMeasure = False
-            for test in request['measures']:
+            for test in request['measure']:
                 if test in measures and station['activity_end'] == None:
                     hasMeasure = True
             
@@ -59,6 +71,24 @@ async def getstation():
                     id = index
 
     if id >= 0:
-        return {'station': stations[id]['name']}
+        return {'station_id': stations[id]['local_id'], 'station_name': stations[id]['name']}
     else:
         return {'station': 'NO STATION FOUND'}
+    
+@app.get('/get-current-station-data')
+async def get_current_station_data(id: str, measure: str):
+    """[summary]
+
+    Args:
+        id (str): [description]
+        measure (str): [description]
+
+    Returns:
+        JSON Object: The most reasent measurement from a stations
+    """    
+    stations_one_day_data = json.loads(requests.get('https://api.ust.is/aq/a/getLatest').text)
+    value = stations_one_day_data[id]['parameters'][measure]['0']['value']
+    measurement_endtime = stations_one_day_data[id]['parameters'][measure]['0']['endtime']
+    unit = stations_one_day_data[id]['parameters'][measure]['unit']
+    
+    return {'station_id': id, 'timestamp': measurement_endtime, 'measurement_variable': measure, 'value': value, 'unit': unit}
